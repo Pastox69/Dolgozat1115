e376c601750b61e17f1435ba41416ecbb07cb8b8
istva <istva@DESKTOP-2CECF5V>
2023-11-15 18:11:50 +0100

    Add project files.

---
+++ vjhsfgvsh/Test.cs
@@ -0,0 +1,26 @@
+﻿using Negyzetdoga;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kockadoga
+{
+    class Teszt
+    {
+        static void Main(string[] args)
+        {
+
+            NegyszogClass szenvedes = new NegyszogClass(9, 8);
+            szenvedes.SetKerulet();
+            szenvedes.SetTerulet();
+
+
+            Console.WriteLine("A négyszög kerülete: {0:0.00}, területe {1:0.00}", szenvedes.getKerulet(), szenvedes.getTerulet());
+
+            Console.ReadKey();
+
+        }
+    }
+}
